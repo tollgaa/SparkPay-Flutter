4620956d52860bd3ceaf58cfd4cd1a695ba19622
Tolga Murat Tor <ttolgamurattor@gmail.com>
2025-01-27 23:46:59 +0300

    Update
    -Kod satırları düzenlendi.

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -5,10 +5,17 @@ const Color ArkaPlanRengi = Color.fromARGB(255, 210, 207, 207);
 const Color KilitEkranArkaPlanRengi = Color.fromARGB(255, 35, 47, 59);
 const Color GunduzMetinRengi = Colors.black;
 const Color GunduzAppBarRengi = Colors.white;
+const Color ArkaPlanRengi = Color.fromARGB(255, 210, 207, 207);  
+const Color KilitEkranArkaPlanRengi = Color.fromARGB(255, 35, 47, 59);  
+const Color GunduzMetinRengi = Colors.black;  
+const Color GunduzAppBarRengi = Colors.white;  
 
 
 const Color GeceArkaPlanRengi = Color.fromARGB(255, 18, 18, 18); // Darker background
 const Color GeceMetinRengi = Colors.white;
 const Color GeceAppBarRengi = Color.fromARGB(255, 33, 33, 33); // Darker app bar
+const Color GeceArkaPlanRengi = Color.fromARGB(255, 18, 18, 18);  
+const Color GeceMetinRengi = Colors.white;  
+const Color GeceAppBarRengi = Color.fromARGB(255, 33, 33, 33);  
 
 const String fontFamily = 'Montserrat';
